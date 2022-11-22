f936d5d50a1441c0d31a3810cccaed7494923b36
Joporezka <alesha.smolnikoff@gmail.com>
2022-11-22 17:24:57 +0300

    Task6 main.cpp

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -8,10 +8,12 @@ int main()
 {
     system("chcp 65001");
     int n,a,b,w;
-    cout<<"Vertex count: ";
+
+    cout<<"Лабораторная работа №6.'Графы'. Смольников Алексей. Вариант 22"<<endl;
+    cout<<"Количество узлов: ";
     cin>>n;
 
-    cout<<"Edge count: ";
+    cout<<"Количество граней: ";
     int e;
     cin>>e;
 
@@ -25,14 +27,18 @@ int main()
         graph.addEdge(a,b,w);
 
     }
+    cout<<"Нахождение мостов в графе: "<<endl;
     graph.findBridges();
+
+    cout<<"Вывод списка смежных вершин"<<endl;
     graph.printGraphList();
     graph.toGraphviz("graph.gv");
+
 
     Graph graph2 = graph.Kruksal();
     graph2.toGraphviz("graphKruksal.gv");
 
-
+    cout<<"Алгоритм Крускала: "<<endl;
     graph2.printTree();
     return 0;
 }
